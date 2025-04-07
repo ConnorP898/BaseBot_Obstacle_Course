0d63a5caaf729ad2532e0d70f0f8261269ebb0c8
ConnorP898 <c.paniura.31415926@gmail.com>
2025-04-07 13:11:24 -0600

    Update BaseBot.py

--- BaseBot.py
+++ BaseBot.py
@@ -1,22 +1,24 @@
+
+#endregion VEXcode Generated Robot Configuration
 myVariable = 0
 
 def when_started1():
     global myVariable
-    drivetrain.drive_for(FORWARD, 300, MM)
+    drivetrain.drive_for(FORWARD, 375, MM)
     drivetrain.turn_for(RIGHT, 90, DEGREES)
-    drivetrain.drive_for(FORWARD, 100, MM)
+    drivetrain.drive_for(FORWARD, 300, MM)
     drivetrain.turn_for(RIGHT, 45, DEGREES)
-    drivetrain.drive_for(FORWARD, 200, MM)
+    drivetrain.drive_for(FORWARD, 320, MM)
     drivetrain.turn_for(LEFT, 45, DEGREES)
+    drivetrain.drive_for(FORWARD, 1000, MM)
+    drivetrain.turn_for(LEFT, 90, DEGREES)
     drivetrain.drive_for(FORWARD, 800, MM)
     drivetrain.turn_for(LEFT, 90, DEGREES)
-    drivetrain.drive_for(FORWARD, 600, MM)
-    drivetrain.turn_for(LEFT, 90, DEGREES)
     drivetrain.drive_for(FORWARD, 200, MM)
-    drivetrain.turn_for(LEFT, 45, DEGREES)
-    drivetrain.drive_for(FORWARD, 150, MM)
-    drivetrain.turn_for(RIGHT, 45, DEGREES)
-    drivetrain.drive_for(FORWARD, 900, MM)
+    drivetrain.turn_for(LEFT, 20, DEGREES)
+    drivetrain.drive_for(FORWARD, 100, MM)
+    drivetrain.turn_for(RIGHT, 20, DEGREES)
+    drivetrain.drive_for(FORWARD, 850, MM)
     drivetrain.turn_for(RIGHT, 90, DEGREES)
     drivetrain.drive_for(FORWARD, 200, MM)
     drivetrain.turn_for(RIGHT, 80, DEGREES)
